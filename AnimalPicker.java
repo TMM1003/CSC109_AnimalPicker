49e9518ec8d659d7d60b46bdbb2cdc312049cd1b
TMM1003 <102240828+TMM1003@users.noreply.github.com>
2026-02-19 19:35:11 -0500

    added checking for invalid user input

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -2,15 +2,19 @@ public class AnimalPicker {
     public static void main(String[] args) {
         java.util.Scanner input = new java.util.Scanner(System.in);
 
-        System.out.print("Which animal would you like to see, a dog or a cat? ");
-        String choice = input.nextLine().toLowerCase();
+        while (true) {
+            System.out.print("Which animal would you like to see, a dog or a cat? ");
+            String choice = input.nextLine().trim().toLowerCase();
 
-        if (choice.equals("cat")) {
-            System.out.println("CAT ASCII ART HERE");
-        } else if (choice.equals("dog")) {
-            System.out.println("DOG ASCII ART HERE");
-        } else {
-            System.out.println("Error: Invalid option. Please choose either \"dog\" or \"cat\".");
+            if (choice.equals("cat")) {
+                System.out.println("CAT ASCII ART HERE");
+                break;
+            } else if (choice.equals("dog")) {
+                System.out.println("DOG ASCII ART HERE");
+                break;
+            } else {
+                System.out.println("Error: Invalid option. Please choose either \"dog\" or \"cat\".");
+            }
         }
 
         input.close();
