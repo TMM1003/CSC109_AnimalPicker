52d437a17aee199b99a15ece6ba3582e5d0ee4c4
TMM1003 <102240828+TMM1003@users.noreply.github.com>
2026-02-19 19:27:43 -0500

    added user input for animal

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,5 +1,18 @@
 public class AnimalPicker {
     public static void main(String[] args) {
-        System.out.println("Initial Commit");
+        java.util.Scanner input = new java.util.Scanner(System.in);
+
+        System.out.print("Which animal would you like to see, a dog or a cat? ");
+        String choice = input.nextLine().toLowerCase();
+
+        if (choice.equals("cat")) {
+            System.out.println("CAT ASCII ART HERE");
+        } else if (choice.equals("dog")) {
+            System.out.println("DOG ASCII ART HERE");
+        } else {
+            System.out.println("Please choose either \"dog\" or \"cat\".");
+        }
+
+        input.close();
     }
 }
